f07e1c879895f508d29784abf1191480720ba480
Arthur Carson <carson.bob.arthur@gmail.com>
2017-12-27 21:26:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -51,6 +51,6 @@ function destructivelyRemoveElementFromEndOfArray(popTest) {
 
 var sliceTest = []
 function removeElementFromEndOfArray(sliceTest) {
-  sliceTest.slice(2)
-  return sliceTest
+  newList = sliceTest.slice(2)
+  return newList
 }
